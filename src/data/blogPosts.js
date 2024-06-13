50b717b19933610432b237f55f3d86bec0c22421
404Nikhil <nicksd111@gmail.com>
2024-06-14 00:45:22 +0530

    tc blog

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -1,8 +1,8 @@
 export const blogPosts = [
     {
-      id: "06-08-2024-epubs",
-      title: "Epubs vs Pdfs",
-      date: "06-08-2024",
+      id: "02-05-2024-tailcall",
+      title: "Tailcall and GraphQl",
+      date: "02-05-2024",
     },
     {
       id: "06-27-2023-npm-and-vite",
@@ -13,6 +13,11 @@ export const blogPosts = [
       id: "06-26-2023-npm-bundler",
       title: "Parcel is the beast",
       date: "06-26-2023",
+    },
+    {
+      id: "06-08-2023-epubs",
+      title: "Epubs vs Pdfs",
+      date: "06-08-2023",
     },
     {
       id: "05-26-2023-server-side",
